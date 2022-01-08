aa19775d665fbc05a4ddbf4a84da175994e8e3b1
Veerababu Garikipati <veerababu.garikipati@yahoo.co.in>
2022-01-09 00:12:10 +0400

    Add files via upload

---
+++ src/main/java/com/spring/azure/demo/HelloWorldController.java
@@ -0,0 +1,10 @@
+package com.spring.azure.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RESTController;
+@RESTController
+public class HelloWorldController {
+@GetMapping(“/hello-world”)
+public String HelloWorld(){
+return “Hello World”;
+}
+}
