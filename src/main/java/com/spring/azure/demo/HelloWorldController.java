a822f0d44562589a2bcef81e369d193e740a02fe
Veerababu Garikipati <veerababu.garikipati@yahoo.co.in>
2022-01-09 00:36:24 +0400

    Update HelloWorldController.java

--- src/main/java/com/spring/azure/demo/HelloWorldController.java
+++ src/main/java/com/spring/azure/demo/HelloWorldController.java
@@ -1,7 +1,7 @@
 package com.spring.azure.demo;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RESTController;
-@RESTController
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class HelloWorldController {
 @GetMapping("/hello-world")
 public String HelloWorld(){
